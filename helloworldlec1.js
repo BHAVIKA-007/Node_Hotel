b077384f78a159d0ce3d70ef1f57c2afd9bb818f
BHAVIKA-007 <bhavikaboddireddy03@gmail.com>
2025-07-03 12:08:19 +0530

    Delete helloworldlec1.js
    Unnecessary file

--- helloworldlec1.js
+++
@@ -1,9 +0,0 @@
-function product(name,price,instock){
-   this.name=name;
-    this.price=price;
-    this.inStock=instock;
-}
-const product1= new product("Earphone",100,true);
-const product2=new product("Laptop",50000,false);
-console.log(product1);
-console.log(product2);
